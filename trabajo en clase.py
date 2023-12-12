0c34744a671a2da97a28e801bde383d3c7ab60b4
Jeremy Sebastian Apuparo Jara <147017392+sebastianjara7@users.noreply.github.com>
2023-12-12 09:58:45 -0500

    Add files via upload

---
+++ trabajo en clase.py
@@ -0,0 +1,50 @@
+# El proposito de este trabajo es aplicar lo aprendido en clase y poder mejorar nuestros conocimientos.
+# Ejercico 1
+# Suma de dos números enteros
+x = 33
+y= 7
+suma = x + y
+
+# Resta de dos números tipo flotante
+a = 6.7
+b = 3.4
+resta = a - b
+
+# Multiplicacion de dos números enteros
+multiplicacion = x * y
+
+# Divicion de dos números de punto flotante
+division = a / b
+
+# Imprecion de los resultados
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicación: {multiplicacion}")
+print(f"División: {division}")
+
+# Ejercicio 2
+# Declaracion de una variable de tipo string con mi nombre completo
+nombre_completo = "LUIS ALEJANDRO ARPI SAQUIPAY"
+
+# Obtencion de  la longitud del nombre completo
+longitud_nombre = len(nombre_completo)
+
+# Imprecion de el nombre completo y su longitud
+print("\nNombre completo:" , nombre_completo)
+print("Longitud del nombre completo:", longitud_nombre)
+
+# Ejerccio 3
+# Declarar una variable entera y otra de punto flotante
+p = 4
+q = 8.8
+
+# Convertir la variable entera a punto flotante y lo mismo con la entera
+flotante_desde_entero = float(p)
+entero_desde_flotante = int(q)
+
+# Imprimir los 2 tipos de datos originales y después la conversión
+print("\nTipo de dato original para 'p':", type(p))
+print("Tipo de dato después de la conversión p:",type(flotante_desde_entero))
+
+print("\nTipo de dato original para 'q':",type(q))
+print("Tipo de dato después de la conversión q:", type(entero_desde_flotante))
